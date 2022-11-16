d143d712c9fbf317503a6394b12d7d9c76784dc9
tszkoda <st.thomasrocks@gmail.com>
2022-11-16 17:14:21 -0500

    very important comment added.

---
+++ src/main/java/net/thomas/szkodamod/SzkodaMod.java
@@ -0,0 +1,15 @@
+package net.thomas.szkodamod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//very important comment
+public class SzkodaMod implements ModInitializer {
+	public static final String MOD_ID = "szkodamod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/thomas/szkodamod/SzkodaModClient.java
@@ -0,0 +1,10 @@
+package net.thomas.szkodamod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SzkodaModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
