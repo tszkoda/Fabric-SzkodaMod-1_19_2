2c1397a01ce92be1451345f01852ccfaed10b34b
tszkoda <55335305+tszkoda@users.noreply.github.com>
2022-11-21 20:07:55 -0500

    New Ore Blocks added:
    -Tungsten Ore
    -Deepslate Tungsten Ore

--- src/main/java/net/thomas/szkodamod/block/ModBlocks.java
+++ src/main/java/net/thomas/szkodamod/block/ModBlocks.java
@@ -4,10 +4,12 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.block.OreBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.util.registry.Registry;
 import net.thomas.szkodamod.SzkodaMod;
 import net.thomas.szkodamod.item.ModItemGroup;
@@ -16,6 +18,14 @@ public class ModBlocks {
 
     public static final Block TUNGSTEN_BLOCK = registerBlock("tungsten_block",
             new Block(FabricBlockSettings.of(Material.METAL).strength(0.1f).requiresTool()), ModItemGroup.TUNGSTEN_INGOT);
+
+    public static final Block TUNGSTEN_ORE = registerBlock("tungsten_ore",
+            new OreBlock(FabricBlockSettings.of(Material.STONE).strength(0.1f).requiresTool(),
+        UniformIntProvider.create(3,7)), ModItemGroup.TUNGSTEN_INGOT);
+
+    public static final Block DEEPSLATE_TUNGSTEN_ORE = registerBlock("deepslate_tungsten_ore",
+            new OreBlock(FabricBlockSettings.of(Material.STONE).strength(0.1f).requiresTool(),
+                    UniformIntProvider.create(3,7)), ModItemGroup.TUNGSTEN_INGOT);
 
     private static Block registerBlock(String name, Block block, ItemGroup tab) {
         registerBlockItem(name, block, tab);
